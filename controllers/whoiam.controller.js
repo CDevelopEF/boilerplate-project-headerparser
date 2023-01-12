f577c787ee2a4d77f77c84ec785508e6b78a5eb5
CDevelopEF <ernestopiz866@gmail.com>
2023-01-12 12:41:54 -0500

    othercommit

--- controllers/whoiam.controller.js
+++ controllers/whoiam.controller.js
@@ -8,9 +8,7 @@ router.get('/whoami', (req, res)=>{
     return res.json({
         ipaddress: req.ip ,
         lenguage: req.rawHeaders[req.rawHeaders.indexOf('Accept-Language')+1],
-        host: req.rawHeaders[req.rawHeaders.indexOf('User-Agent')+1],
-    
-
+        software: req.rawHeaders[req.rawHeaders.indexOf('User-Agent')+1],
     });
 })
 
