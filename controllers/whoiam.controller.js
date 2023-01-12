0f9cc0920a968d3ffdd477958e879206ca599191
CDevelopEF <ernestopiz866@gmail.com>
2023-01-12 12:30:34 -0500

    fixed2

--- controllers/whoiam.controller.js
+++ controllers/whoiam.controller.js
@@ -4,7 +4,7 @@ const system = require('node:os')
 const net    = require('node:net');
 const router = app.Router();
 
-router.get('/whoiam', (req, res)=>{
+router.get('/whoami', (req, res)=>{
     return res.json({
         ipaddress: req.ip ,
         lenguage: req.rawHeaders[req.rawHeaders.indexOf('Accept-Language')+1],
